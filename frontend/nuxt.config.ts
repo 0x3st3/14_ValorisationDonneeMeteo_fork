f32b588527d2acc6d78923d2a62008ed0ac01a14
rnoyer <romain.noyer@gmail.com>
2026-02-26 12:13:56 +0100

    feat: remove nuxt.config as it is handled in map directly

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -37,10 +37,6 @@ export default defineNuxtConfig({
             },
             link: [
                 { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-                {
-                    rel: "stylesheet",
-                    href: "https://unpkg.com/maplibre-gl@5.18.0/dist/maplibre-gl.css",
-                },
             ],
         },
     },
